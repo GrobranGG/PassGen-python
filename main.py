80f6272fd3d011364af0dba0c7749138f425d42b
Lev <grobran.gg@gmail.com>
2021-11-01 12:45:57 +0500

    PassGen-python v.1.2 release
    PassGen-python v.1.2 release

--- main.py
+++ main.py
@@ -1,19 +1,27 @@
-# PassGen (v.1.1) by GrobranGG
+# PassGen (v.1.2) by GrobranGG
 import random
 import colorama
 import secrets
 import os
 
+os.system("clear")
+os.system("cls")
 from colorama import init
 from colorama import Fore, Back
 from random import randint
 init()
-os.system("clear")
-os.system("cls")
 
-print(Fore.CYAN + "PassGen v.1.1 (by GrobranGG)")
-print(Back.WHITE)
-print(Fore.BLACK + "Choose a language (number):")
+# Banner
+print(Fore.GREEN + " _____               _____            ")
+print(Fore.GREEN + "|  __ \             / ____|           ")
+print(Fore.GREEN + "| |__) |_ _ ___ ___| |  __  ___ _ __  ")
+print(Fore.GREEN + "|  ___/ _` / __/ __| | |_ |/ _ \ '_ \ ")
+print(Fore.GREEN + "| |  | (_| \__ \__ \ |__| |  __/ | | |")
+print(Fore.GREEN + "|_|   \__,_|___/___/\_____|\___|_| |_|")
+print(Fore.RED + "                  v.1.2 (by GrobranGG)\n")
+
+# Choose a language
+print(Fore.WHITE + "Choose a language (number):")
 print("1. English")
 print("2. Russian")
 
@@ -22,11 +30,14 @@ print(Fore.CYAN)
 language = input("Enter your language: ")
 chars = '+-/*!&$#?=@<>abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
 
+# Password generation
 if language == "1":
     number = int(input("How many passwords do you want to generate?: "))
     length = int(input("What password length do you want to get?: "))
 
-    print(Fore.GREEN + "Your password has been generated! Result:")
+    os.system("clear")
+    os.system("cls")
+    print(Fore.GREEN + "Your passwords are generated! Result:")
     print(Fore.WHITE)
     for n in range(number):
         password = ''
@@ -38,7 +49,9 @@ elif language == "2":
     number = int(input("Сколько паролей вы хотите сгенирировать?: "))
     length = int(input("Какую длину пароля вы хотите получить?: "))
 
-    print(Fore.GREEN + "Ваш пароль сгенерирован! Результат:")
+    os.system("clear")
+    os.system("cls")
+    print(Fore.GREEN + "Ваши пароли сгенерированы! Результат:")
     print(Fore.WHITE)
     for n in range(number):
         password = ''
