566228f8cfebb4b0950d7bd92751838ac654c7fd
Lev <grobran.gg@gmail.com>
2021-11-02 21:03:40 +0500

    PassGen-python v.1.3 update
    In this update, I have optimized the code.

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
-# PassGen (v.1.2) by GrobranGG
+# PassGen (v.1.3) by GrobranGG
 import random
 import colorama
-import secrets
 import os
+import sys
 
-os.system("clear")
-os.system("cls")
-from colorama import init
-from colorama import Fore, Back
-from random import randint
-init()
+opersystem = sys.platform
+
+if opersystem == "win32":
+    os.system("cls")
+elif opersystem == 'linux':
+    os.system("clear")
+
+from colorama import Fore
+colorama.init()
 
 # Banner
 print(Fore.GREEN + " _____               _____            ")
@@ -18,47 +21,43 @@ print(Fore.GREEN + "| |__) |_ _ ___ ___| |  __  ___ _ __  ")
 print(Fore.GREEN + "|  ___/ _` / __/ __| | |_ |/ _ \ '_ \ ")
 print(Fore.GREEN + "| |  | (_| \__ \__ \ |__| |  __/ | | |")
 print(Fore.GREEN + "|_|   \__,_|___/___/\_____|\___|_| |_|")
-print(Fore.RED + "                  v.1.2 (by GrobranGG)\n")
+print(Fore.RED + "                  v.1.3 (by GrobranGG)\n")
 
 # Choose a language
 print(Fore.WHITE + "Choose a language (number):")
 print("1. English")
 print("2. Russian")
 
-print(Back.RESET)
 print(Fore.CYAN)
 language = input("Enter your language: ")
+
+english = {'how many passwords': 'How many passwords do you want to generate?: ', 'length': 'What password length do you want to get?: ', 'generated': 'Your passwords are generated! Result:'}
+russian = {'how many passwords': 'Сколько паролей вы хотите сгенирировать?: ', 'length': 'Какую длину пароля вы хотите получить?: ', 'generated': 'Ваши пароли сгенерированы! Результат:'}
+if language == "1":
+    lang = english
+elif language == "2":
+    lang = russian
+else:
+    print(Fore.RED + "Incorrect language!")
+
 chars = '+-/*!&$#?=@<>abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
 
 # Password generation
-if language == "1":
-    number = int(input("How many passwords do you want to generate?: "))
-    length = int(input("What password length do you want to get?: "))
+number = int(input(lang['how many passwords']))
+length = int(input(lang['length']))
 
-    os.system("clear")
+if opersystem == "win32":
     os.system("cls")
-    print(Fore.GREEN + "Your passwords are generated! Result:")
-    print(Fore.WHITE)
-    for n in range(number):
-        password = ''
-        for i in range(length):
-            password += random.choice(chars)
-        print(password)
-
-elif language == "2":
-    number = int(input("Сколько паролей вы хотите сгенирировать?: "))
-    length = int(input("Какую длину пароля вы хотите получить?: "))
-
+elif opersystem == 'linux':
     os.system("clear")
-    os.system("cls")
-    print(Fore.GREEN + "Ваши пароли сгенерированы! Результат:")
-    print(Fore.WHITE)
-    for n in range(number):
-        password = ''
-        for i in range(length):
-            password += random.choice(chars)
-        print(password)
-else:
-    print(Fore.RED + "Incorrect language!")
+
+print(Fore.GREEN)
+print(lang['generated'])
+print(Fore.WHITE)
+for n in range(number):
+    password = ''
+    for i in range(length):
+        password += random.choice(chars)
+    print(password)
 
 input()
